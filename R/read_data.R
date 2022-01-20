1c9ac86f84eb6f21040b0347e01587139d67463b
Remko Duursma <remkoduursma@gmail.com>
2022-01-20 13:53:51 +0100

    buurt code fixen

--- R/read_data.R
+++ R/read_data.R
@@ -492,7 +492,7 @@ read_brpstam <- function(brp_bzsprs, adressen_inst, peil_datum, date_format = c(
 add_buurt_wijk_columns <- function(data){
   
   # foute levering 2022-01-20
-  if(length(unique(data$buurt_code_cipers)) < 20){
+  if(length(unique(data$buurt_code_cipers)) < 30){
     
     data(buurt_koppel_fix_pink)
     data$buurt_code_cipers <- NULL
