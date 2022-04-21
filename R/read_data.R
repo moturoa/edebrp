3d45473aa24668b056dcae39a47a6c1127030282
Remko Duursma <remko@shintolabs.nl>
2022-04-21 08:53:32 +0200

    wijk codes iets andere levering

--- R/read_data.R
+++ R/read_data.R
@@ -515,7 +515,7 @@ add_buurt_wijk_columns <- function(data){
     relocate(buurt_code, .after = buurt_naam) %>%
     mutate(buurt_code_cbs  = paste0("BU0228", buurt_code)) %>%
     relocate(buurt_code_cbs, .after = buurt_code) %>%
-    mutate(wijk_code_cbs  = paste0("WK0228", substr(wijk_code,2,3))) %>%
+    mutate(wijk_code_cbs  = paste0("WK0228", str_pad(wijk_code, width = 2, pad = "0"))) %>%
     relocate(wijk_code_cbs, .after = wijk_code) %>%
     relocate(buurt_code_cipers, .after = buurt_code)
   
